8dd3cc9375595ad51f1fac53166162fde7024441
phan.dang.hai.vu <SUN-ASTERISK\phan.dang.hai.vu@b121968-pc.sun-asterisk.com>
2019-11-12 14:29:09 +0700

    send mail by job

--- app/Listeners/MakeInitComment.php
+++ app/Listeners/MakeInitComment.php
@@ -3,10 +3,12 @@
 namespace App\Listeners;
 
 use App\Events\PostCreated;
+use App\Mail\Commented;
 use App\Model\Comment;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class MakeInitComment
 {
@@ -29,5 +31,6 @@ class MakeInitComment
     public function handle(PostCreated $event)
     {
         Comment::create(['user_id' => Auth::id(), 'post_id' => $event->post->id, 'content' => 'Init comment made by event']);
+        Mail::to($event->post->user->email)->send(new Commented($event->post));
     }
 }
